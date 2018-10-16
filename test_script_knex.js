a8ec209baf5a132bef6ca1e90085d85067182f03
Ianden <landendanyluk@gmail.com>
2018-10-16 14:18:22 -0400

    Add knex integration

---
+++ test_script_knex.js
@@ -0,0 +1,37 @@
+const settings = require("./settings"); 
+
+let knex = require ('knex')({
+  client: 'pg',
+  connection: {
+    host : settings.hostname,
+    user : settings.user,
+    password : settings.password,
+    database : settings.database
+  }
+});
+
+function findFamousByName(name) {
+  knex('famous_people')
+    .where({'first_name': name})
+    .then(rows =>
+        rows.forEach((r, i) => 
+          console.log(`${i + 1}. ${r.first_name} ${r.last_name}, born ${r.birthdate}`)
+        )
+      );
+  }
+
+function addFamousPerson(person) {
+  knex('famous_people')
+    .insert({
+      first_name: person.first_name,
+      last_name: person.last_name,
+      birthdate: person.birthdate 
+    }).then( res => console.log('ok'));
+}
+
+const newPerson = {first_name: 'Landen',
+                  last_name: 'Danyluk',
+                  birthdate: 'now'}
+
+addFamousPerson(newPerson);
+findFamousByName('Landen');
